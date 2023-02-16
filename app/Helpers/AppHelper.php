267998bead5fdf4b437cd2d80a194cc9b9bf2ed7
Muhammad Noman <muhammad.noman@laksol.com>
2023-02-16 08:12:22 +0500

    Attendence Report Day Correction

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -11,15 +11,14 @@ class AppHelper
         return new AppHelper();
     }
 
-    //$year = 2013, $month = 6, $ignore = [6,1]
+    //$year = 2013, $month = 6, $ignore = [6,0]
     function countDays($year, $month, $ignore = []) {
-        array_push($ignore,6,1); // 6 and 1 are saturday and sunday
+        array_push($ignore,6,0); // 6 and 0 are saturday and sunday
         $holidayList = Holidays::where(['year'=>$year,'month'=>$month])->pluck('date')->toArray();
         
         $count = 0;
         $counter = mktime(0, 0, 0, $month, 1, $year);
         while (date("n", $counter) == $month) {
-            
             if (in_array(date("w", $counter), $ignore) == false && in_array(date("Y-m-d",$counter),$holidayList) == false) {
                 $count++;
             }
